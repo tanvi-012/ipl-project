c39deb70903ed2d12dba089825c435d231b0191a
tanvi-012 <tanvi.sandeep@nineleaps.com>
2023-12-28 11:01:24 +0530

    Admin dashboard integrated with backend

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,36 @@ from flask import Flask, request, jsonify
 import mysql.connector
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from pulp import *
+import warnings
+import pandas as pd
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+import json
+from decimal import Decimal
+
+from flask import Flask, request, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+from pulp import *
+import warnings
+import pandas as pd
+
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+import json
+from decimal import Decimal
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
 
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
+
+
+constraints_csk = ["hgi_csk", [2, 4], [0, 2], [1, 4], [0, 1], [0, 1], [0, 3], [1, 3], [0, 6], [0, 3140], [0, 2]]
 
 # MySQL configuration
 mysql_config = {
@@ -21,8 +46,10 @@ def create_connection():
     connection = mysql.connector.connect(**mysql_config)
     return connection
 
+##################### SHOWING DIFFERENT CATEGORIES PLAYERS ON ADMIN PAGE ###################################
+
 # Route to handle the SQL query with category parameter
-@app.route('/execute-query', methods=['GET'])
+@app.route('/execute_query', methods=['GET'])
 def execute_query():
     try:
         connection = create_connection()
@@ -31,17 +58,19 @@ def execute_query():
         # Get the player category from the query parameters
         category = request.args.get('category', 'Batsman')
 
-        # Using an f-string in the query
-        if category=="Batsman":
-            query = f"SELECT PlayerID as Batsman_ID, PlayerName as Name,Average,StrikeRate as Strike_Rate FROM IPL WHERE Type like '%{category}';"
-        elif category=="Bowler":
-            query = f"SELECT PlayerID as Bowler_ID, PlayerName as Name,BowlingAverage as Average,BowlingStrikeRate as Strike_Rate  FROM IPL WHERE Type like '%{category}';"
-        elif category=="All Rounder":
-            query = f"SELECT PlayerID as All_Rounder_ID, PlayerName as Name,Average as Batting_Average,StrikeRate as Batting_Strike_Rate,BowlingAverage as Bowling_Average,BowlingStrikeRate as Bowling_Strike_Rate FROM IPL WHERE Type like '%{category}';"
-        elif category=="Batsman(w/k)":
-            query = f"SELECT PlayerID as Wicket_Keeper_ID,PlayerName as Name,Average,StrikeRate as Strike_Rate FROM IPL WHERE Type like '%{category}';"
+        query_unsold = ""
 
-        cursor.execute(query)
+        # Using an f-string in the query
+        if category == "Batsman":
+            query_unsold = f"SELECT Player_id,Name,Total_Runs,Overall_Avg as Average,Overall_Strike_rate as Strike_Rate FROM Player_Details where Batsman=1;"
+        elif category == "Bowler":
+            query_unsold = f"SELECT Player_id ,Name,Total_wick as Total_Wickets, bowling_average as Average,economy as Economy FROM Player_Details WHERE Bowler=1;"
+        elif category == "All Rounder":
+            query_unsold = f"SELECT Player_id,Name,Total_Runs,Overall_Avg as Batting_Average,Overall_Strike_rate as Batting_Strike_Rate,Total_wick as Total_Wickets,bowling_average as Bowling_Average,economy as Economy FROM Player_Details where Allrounder=1;"
+        elif category == "Batsman(w/k)":
+            query_unsold = f"SELECT Player_id,Name,Total_Runs, Overall_Avg as Average,Overall_Strike_rate as Strike_Rate FROM Player_Details WHERE wicket_keeper=1;"
+
+        cursor.execute(query_unsold)
         data = cursor.fetchall()
 
         return jsonify(data)
@@ -55,6 +84,115 @@ def execute_query():
         # Log the general exception for debugging purposes
         print(f"Unexpected Error: {str(e)}")
         return jsonify({'error': 'Internal Server Error'})
+
+    finally:
+        # Always close the cursor and connection
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+connection = create_connection()
+cursor = connection.cursor(dictionary=True)
+
+#################################### GETTING BASE PRICE OF EACH PLAYER FROM PLAYER ID #############################3
+
+@app.route('/get_bid_amount', methods=['GET'])
+def get_bid_amount():
+    try:
+        # Get player_id from the query parameters
+        player_id = int(request.args.get('player_id', 162))
+
+        # Establish a database connection
+        connection = mysql.connector.connect(**mysql_config)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        # Use a parameterized query to fetch the bid amount for the specified player_id
+        query = "SELECT min_price,max_price FROM Player_Details WHERE Player_id = %s"
+        cursor.execute(query, (player_id,))
+
+        # Fetch the result
+        bid_amount = cursor.fetchone()
+
+        # Close the cursor and the database connection
+        cursor.close()
+        connection.close()
+
+        if bid_amount is not None:
+            return jsonify({"min bid_amount": bid_amount[0],
+                            "max bid_amount": bid_amount[1]})
+        else:
+            return jsonify({"error": "Bid amount not found for the specified player_id"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+######################################## GETTING THE REMAINING PURSE FOR THE TEAM #################################
+
+@app.route('/user/getpurse', methods=["GET"])
+def user_getpurse_model():
+    try:
+        # Assuming the team information is in the JSON form in the request body
+        request_data = request.json
+        team_name = request_data.get('team')
+
+        if not team_name:
+            return jsonify({"error": "Team name not provided"}), 400
+
+        # Establish a database connection
+        connection = mysql.connector.connect(**mysql_config)
+
+        # Create a cursor
+        cursor = connection.cursor()
+
+        query = "SELECT COALESCE(10000 - SUM(Price), 10000) AS remaining_purse FROM Retained_Players WHERE Team = %s GROUP BY Team"
+        cursor.execute(query, (team_name,))
+        result = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        if result:
+            result_dict = {'remaining_purse': float(result[0])}
+            return jsonify(result_dict)
+        else:
+            return jsonify({"error": "Remaining purse not found for the specified team"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+######################################### PUTTING PLAYER INFORMATION IN THE RETAINED TABLE ############################
+
+@app.route('/player_data_post', methods=['POST'])
+def handle_post_request():
+    try:
+        # Check if the request has JSON data
+        if not request.json:
+            return jsonify({'error': 'Invalid JSON format'}), 400
+
+        # Get data from the request JSON
+        data = request.json
+
+        # Perform operations with the received data
+        # ...
+        cursor.execute("INSERT INTO Retained(`Player Name`, Team, Price) VALUES (%s, %s, %s)",
+                       (data.get('name'), data.get('team'), data.get('price')))
+        connection.commit()
+
+        return jsonify({'success': 'Data added successfully'})
+
+    except Exception as e:
+        # Log the error for debugging purposes
+        print(f"Unexpected Error: {str(e)}")
+        return jsonify({'error': 'Internal Server Error'})
+
+    finally:
+        # Always close the cursor and connection
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
 
 @socketio.on('running')
 def handle_start(running):
--- src/Components/admin_dashboard/admin_dashboard.js
+++ src/Components/admin_dashboard/admin_dashboard.js
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MDBRow, MDBCol,MDBBtn,MDBContainer, MDBBtnGroup ,MDBCard,
-    MDBCardHeader,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText} from 'mdb-react-ui-kit';
-
+import { MDBRow, MDBCol, MDBBtn, MDBContainer, MDBBtnGroup, MDBCard, MDBCardHeader, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
 import axios from 'axios';
 import TimerComponent from '../timer';
 import PlayerCard from '../playerCard';
@@ -16,92 +11,168 @@ const socket = io.connect("http://127.0.0.1:5000");
 
 
 export default function AdminDashboard(){
-    const [isCardVisible, setCardVisibility]= useState(false);
-    const [isRunning,setIsRunning]=useState(0);
-    const [data, setData] = useState(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [allPlayersFinished, setAllPlayersFinished] = useState(false);
-    
+  const [isCardVisible, setCardVisibility]= useState(false);
+  const [isRunning,setIsRunning]=useState(0);
+  const [data, setData] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [allPlayersFinished, setAllPlayersFinished] = useState(false);
 
-    useEffect(() =>{
-      socket.on('running',(running) => {
-        console.log('running:',running);
-        setIsRunning(running);
-        if(running===0){setCardVisibility(false)}
+  const Player_id = 1;
+  useEffect(() =>{
+    socket.on('running',(running) => {
+      console.log('running:',running);
+      setIsRunning(running);
+      if(running===0){setCardVisibility(false)}
+    })
+    return () =>{
+     
+    };
+  },[]);
+
+  const handleCardClick = () => {
+    setCardVisibility(!isCardVisible);
+  };
+
+  const fetchData = async (category) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/execute-query?category=${category}`);
+      setData(response.data);
+      socket.emit('data',data);
+      setAllPlayersFinished(false);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    axios.delete(`http://localhost:5000/player_data_delete?id=${Player_id}`)
+      .then(response => {
+        console.log('Data deleted successfully:', response.data);
       })
-      return () =>{
-       
-      };
-    },[]);
+      .catch(error => {
+        console.error('Error deleting data:', error);
+      });
+  }, [Player_id]);
 
-    const handleCardClick = () => {
-        setCardVisibility(!isCardVisible);
-    };
+  const [remainingPurse, setRemainingPurse] = useState(null);
 
-    const fetchData = async (category) => {
+  useEffect(() => {
+    const fetchPurseData = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/execute-query?category=${category}`);
-        setData(response.data);
-        socket.emit('data',data);
-        setAllPlayersFinished(false);
+        const teamName = 'YourTeamName';
+        const response = await axios.get(`http://localhost:5000/user/getpurse?team=${teamName}`);
+        const { remaining_purse: remainingPurseValue } = response.data;
+        setRemainingPurse(remainingPurseValue);
       } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error fetching purse data:', error);
       }
     };
-  
-    const handleButtonClick = (category) => {
-      setCardVisibility(true);
-      setSelectedCategory(category);
-      socket.emit('category',category);
-      if (!data || currentIndex === data.length || category !== selectedCategory) {
-        fetchData(category);
-        setCurrentIndex(0);
-      } else {
-        setCurrentIndex(currentIndex + 1);
-      }
-      socket.emit('index',currentIndex);
-    };
-  
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    useEffect(() => {
-      if (data && currentIndex >= data.length &&currentIndex!=0) {
-        setAllPlayersFinished(true);
-        setCardVisibility(false);
-      } else {
-        setAllPlayersFinished(false);
-      }
-    }, [data, currentIndex]);
 
-    
-    return (
+    fetchPurseData();
+  }, []);
+
+  useEffect(() => {
+    axios.post('http://localhost:5000/player_data_post', {
+      name: 'John Doe',
+      team: 'Some Team',
+      price: 1000000
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        console.log('Data posted successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error posting data:', error);
+      });
+  }, []);
+
+  const [teamData, setTeamData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/team/csk');
+        setTeamData(response.data);
+      } catch (error) {
+        console.error('Error fetching team data:', error);
+        setError('Error fetching team data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const [selectedTeamData, setSelectedTeamData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://your-flask-server-hostname-or-ip/user/selectedteam/csk');
+        setSelectedTeamData(response.data.players);
+      } catch (error) {
+        console.error('Error fetching selected team data:', error);
+        setError('Error fetching selected team data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleButtonClick = (category) => {
+    setCardVisibility(true);
+    setSelectedCategory(category);
+    socket.emit('category',category);
+    if (!data || currentIndex === data.length || category !== selectedCategory) {
+      fetchData(category);
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+    socket.emit('index',currentIndex);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data && currentIndex >= data.length && currentIndex !== 0) {
+      setAllPlayersFinished(true);
+      setCardVisibility(false);
+    } else {
+      setAllPlayersFinished(false);
+    }
+  }, [data, currentIndex]);
+
+  return (
     <MDBContainer className='my-5'>
-        <MDBRow className='g-0 d-flex align-items-center'>
-            <MDBCol md='6'><div>Bid Amount| Bid Team</div></MDBCol>
-            <MDBCol md='6'>
+      <MDBRow className='g-0 d-flex align-items-center'>
+        <MDBCol md='6'><div>Bid Amount| Bid Team</div></MDBCol>
+        <MDBCol md='6'>
             <TimerComponent></TimerComponent>
-            </MDBCol>
-        </MDBRow>
-        <MDBRow className='g-0 d-flex align-items-center' style={{paddingTop:'20px',paddingBottom:'20px'}}>
-            <MDBBtnGroup shadow='0'>
-            <MDBBtn color='secondary' disabled={isRunning} onClick={handleCardClick} style={{margin:'4px'}}>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className='g-0 d-flex align-items-center' style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+        <MDBBtnGroup shadow='0'>
+          <MDBBtn color='secondary' disabled={isRunning} onClick={() => { handleCardClick(); handleButtonClick("Batsman"); }} style={{ margin: '4px' }}>
             Batsman
-            </MDBBtn>
-            <MDBBtn color='secondary' disabled={isRunning} onClick={handleCardClick} style={{margin:'4px'}}>
+          </MDBBtn>
+          <MDBBtn color='secondary' disabled={isRunning} onClick={() => { handleCardClick(); handleButtonClick("Bowler"); }} style={{ margin: '4px' }}>
             Bowler
-            </MDBBtn>
-            <MDBBtn color='secondary' disabled={isRunning} onClick={handleCardClick}  style={{margin:'4px'}}>
+          </MDBBtn>
+          <MDBBtn color='secondary' disabled={isRunning} onClick={() => { handleCardClick(); handleButtonClick("All Rounder"); }} style={{ margin: '4px' }}>
             All Rounder
-            </MDBBtn>
-            <MDBBtn color='secondary' disabled={isRunning} onClick={handleCardClick}  style={{margin:'4px'}}>
-            Wicketkeeper
-            </MDBBtn>
+          </MDBBtn>
+          <MDBBtn color='secondary' disabled={isRunning} onClick={() => { handleCardClick(); handleButtonClick("Batsman(w/k)"); }} style={{ margin: '4px' }}>
+            Wicket Keeper
+          </MDBBtn>
         </MDBBtnGroup>
       </MDBRow>
-      
       <MDBRow className='g-0 d-flex align-items-center' style={{ paddingTop: '20px', paddingBottom: '20px' }}>
         <MDBCol className='d-flex align-items-center'>
         <PlayerCard
@@ -119,4 +190,5 @@ export default function AdminDashboard(){
         </MDBCol>
       </MDBRow>
     </MDBContainer>
-  );}
+  );
+}
